b8afbe29dd1d2d346b8c4bde44ef1a5a73133d8e
mohammedjm99 <mohammedjmohammed111@gmail.com>
2023-04-01 10:40:26 +0300

    backend ready to deploy

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://172.20.10.4:3000", "http://172.20.10.4:3001"],
+    origin: ["https://ecerest.onrender.com", "https://ecerest2.onrender.com"],
     methods: ["GET", "POST"],
   },
 });
