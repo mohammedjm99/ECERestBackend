2d4ec5a07eaef2db53e9b0d0dd059480a9de385c
mohammedjm99 <mohammedjmohammed111@gmail.com>
2023-04-02 08:57:58 +0300

    backend ready to deploy

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 3002;
+const http = require("http");
 const cors = require("cors");
-const mongoose = require('mongoose');
+const { Server } = require("socket.io");
 
 const allowedOrigins = [
-  'https://ecerest.onrender.com',
-  'https://ecerest2.onrender.com'
+  'http://localhost:3000',
+  'http://localhost:3001'
 ]
-
 app.use(cors({
   origin: (origin, callback) => {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
@@ -22,12 +22,71 @@ app.use(cors({
   optionsSuccessStatus: 200
 }));
 app.use(express.json());
+const mongoose = require('mongoose');
 
 const tableRouter = require('./routes/table');
 const authRouter = require('./routes/auth');
 const orderRouter = require('./routes/order');
 const managerRouter = require('./routes/manager');
 const productRouter = require('./routes/product');
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: allowedOrigins,
+    methods: ["GET", "POST"],
+  },
+});
+
+let users = [];
+
+const addUser = (userId, socketId, rule) => {
+  users.push({ rule, userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+
+  socket.on("joinUserO", userId => {
+    addUser(userId, socket.id, "user");
+    console.log(users);
+    console.count('joinUser');
+  });
+
+  socket.on("joinChief", userId => {
+    addUser(userId, socket.id, "chief");
+    console.log(users);
+    console.count('joinChief');
+  });
+
+  socket.on("joinAdmin", userId => {
+    addUser(userId, socket.id, "admin");
+    console.log(users);
+    console.count('joinAdmin');
+  });
+
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+    console.log(users);
+    console.count('disconnect');
+  });
+
+  socket.on("addOrder", data => {
+    for (const user of users) {
+      if (user.rule !== 'user') socket.to(user.socketId).emit("addOrder", data)
+    }
+  })
+
+
+});
 
 app.use('/api/table', tableRouter);
 app.use('/api/auth', authRouter);
@@ -36,7 +95,7 @@ app.use('/api/manager', managerRouter);
 app.use('/api/product', productRouter);
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
-  app.listen(PORT, () => {
+  server.listen(PORT, () => {
     console.log("SERVER RUNNING ON PORT", PORT, "AND DB CONNECTED...");
   });
 })
